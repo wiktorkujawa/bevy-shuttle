9553a6a8299b3df8fce11932b2d9a2939614d4cb
wiktorkujawa <wiktorkujawa1993@gmail.com>
2024-06-16 21:54:50 +0200

    add cors

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,13 @@
 use axum::Router;
 use tower_http::services::{ServeDir, ServeFile};
+use tower_http::cors::{Any, CorsLayer};
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
+    let layer = CorsLayer::new().allow_headers(Any);
     let router = Router::new().nest_service(
         "/",
         ServeDir::new("dist").not_found_service(ServeFile::new("dist/index.html")),
-    );
+    ).layer(layer);
     Ok(router.into())
 }
