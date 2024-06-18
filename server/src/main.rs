3de3b814e510dddeadcd501831c5164c66df557d
wiktorkujawa <wiktorkujawa1993@gmail.com>
2024-06-18 21:12:22 +0200

    change cors to allow all headers, methods and origins

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,11 @@
 use axum::Router;
 use tower_http::services::{ServeDir, ServeFile};
-use tower_http::cors::{Any, CorsLayer};
+use tower_http::cors::CorsLayer;
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let layer = CorsLayer::new().allow_headers(Any);
+    // let layer = CorsLayer::new().allow_headers(Any);
+    let layer = CorsLayer::permissive();
     let router = Router::new().nest_service(
         "/",
         ServeDir::new("dist").not_found_service(ServeFile::new("dist/index.html")),
